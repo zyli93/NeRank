610aabfee4bc9043aabb625e8d358df97efbaf15
SuperBlee <purillmit@gmail.com>
2018-02-12 17:24:52 -0800

    Add modified walk

--- src/generate_walk.py
+++ src/generate_walk.py
@@ -26,7 +26,6 @@ class RandomWalkGenerator:
         self._num_walks = num_walks
         self.G = nx.Graph()
 
-
     def initialize(self, uqq_file, qua_file):
         """ Initialize Graph
 
@@ -39,7 +38,6 @@ class RandomWalkGenerator:
 
         """
         G = self.G
-
         # Read in Uq-Q pairs
         with open(uqq_file, "r") as fin:
             lines = fin.readlines()
@@ -49,7 +47,6 @@ class RandomWalkGenerator:
                 RQ_edge_list.append(["R_" + unit[0],
                                      "Q_" + unit[1]])
             G.add_edges_from(RQ_edge_list)
-
         with open(qua_file, "r") as fin:
             lines = fin.readlines()
             QA_edge_list = []
@@ -59,14 +56,35 @@ class RandomWalkGenerator:
                                      "A_" + unit[1]])
             G.add_edges_from(QA_edge_list)
 
-
-    def generate_walk(self):
-        """ Generate Random Walk
-
-        Generating random walk from the Tripatite graph
+    def get_nodelist(self, type=None):
+        """ Get specific type of nodelist in the graph
 
         Args:
+            type - the entity type of the entity
 
+        Return:
+            nodelist - the list of node with `type`
+        """
+        G = self.G
+        if not type:
+            return list(G.nodes)
+        return [node for node in list(G.nodes)
+                if node[0] == type]
+
+    def generate_walks(self, patterns, alpha):
+        """ Generate Random Walk
+
+        Generating random walk from the Tripartite graph
+        A candidate pattern pool is:
+            "A-Q-R-Q-A": specifies 2 A's answered a question proposed by a same R
+            "A-Q-A": speficies 2 A' answered a same question
+
+        Args:
+            meta_pattern - the pattern that guides the walk generation
+            alpha - probability of restart
+
+        Return:
+            walks - a set of generated random walks
         """
         G = self.G
         num_walks, walk_len = self._num_walks, self._walk_length
@@ -78,24 +96,40 @@ class RandomWalkGenerator:
         walks = []
         nodes = list(G.nodes)
 
-        for cnt in range(num_walks):
-            rand.shuffle(nodes)
-            for node in nodes:
-                walks.append(random_walk(start=node, alpha=alpha))
-
+        for meta_pattern in patterns:
+            for cnt in range(num_walks):
+                nodes = self.get_nodelist()
+                rand.shuffle(nodes)
+                for node in self.get_nodelist("R"):
+                    walks.append(
+                        self.__meta_path_walk(
+                            start=node, alpha=alpha,
+                            pattern=meta_pattern,
+                            rand=rand))
         return walks
 
-
-    def random_walk(self, start=None):
+    def __meta_path_walk(self, rand, start,
+                         alpha=0.0, patterns="AQRQA", walk_len=50):
         """Single Walk Generator
 
-        Generating a single random walk that follows a meta path of
-        `... R - Q - A - Q - R - Q - A - Q - R - Q ...`
+        Generating a single random walk that follows a meta path of `pattern`
 
         Args:
+            rand - an random object to generate random numbers
             start - starting node
+            alpha - probability of restarts
+            pattern - the pattern according to which to generate walks
+            walk_len - the length of the generated walk
+
+        Return:
+            walk - the single walk generated
 
         """
+        G = self.G
+
+
+
+
 
         return walks
 
