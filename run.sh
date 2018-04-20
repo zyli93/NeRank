7ede2bdb6cfe1166d3e28ccf448ff592f54b06c7
Zeyu Li <zyli@cs.ucla.edu>
2018-04-19 20:10:34 -0700

    forget to add run

--- run.sh
+++ run.sh
@@ -11,10 +11,11 @@ else
     PY=/home/zeyu/anaconda3/bin/python
 fi
 
-$PY src/main.py --dataset 3dprinting \
+$PY src/main.py --dataset superuser \
         --window-size 3 --neg-ratio 3.0 --embedding-dim 300 \
-        --lstm-layers 3 --epoch-number 1000 --batch-size 5 --learning-rate 0.01\
+        --lstm-layers 3 --epoch-number 1000 --batch-size 5 --learning-rate 0.01 --cnn-channel 32\
         --gen-metapaths --length 15 --coverage 3 --alpha 0.0 --metapaths "AQRQA" \
         --preprocess \
+        --test-threshold 4 --proportion-test 0.1
 
 
