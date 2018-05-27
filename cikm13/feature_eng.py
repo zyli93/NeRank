47aa1ff9f374f4391f42eede79aa3a7bb0c0092d
Zeyu Li <zeyuli@ucla.edu>
2018-05-26 20:45:09 -0700

    baseline

--- cikm13/feature_eng.py
+++ cikm13/feature_eng.py
@@ -19,20 +19,21 @@ except:
 
 from preprocessing import clean_str2, remove_stopwords
 
+import nltk
 nltk.data.path.append("/workspace/nltk_data")
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
+
+import _pickle as pickle
 
 # Global Variables
 DATA_DIR = os.getcwd() + "/data/"
 PARSED_DIR = DATA_DIR + "parsed/"
 CUR_DIR = os.getcwd() + "/cikm13/"
-CUR_PARSED_DIR = CUR_DIR + "/parsed/"
 
 
 def question_title_len(dataset):
     infile = PARSED_DIR + "{}/Q_title.txt".format(dataset)
-    outfile = CUR_DIR + "question.title.length"
+    outfile = CUR_DIR + "{}/question.title.length".format(dataset)
     with open(infile, "r") as fin,\
         open(outfile, "w") as fout:
         lines = fin.readlines()
@@ -43,7 +44,7 @@ def question_title_len(dataset):
 
 def user_specific(dataset):
     infile = DATA_DIR + "{}/Record_Train.json".format(dataset)
-    outfile = CUR_DIR + "user.specific"
+    outfile = CUR_DIR + "{}/user.specific".format(dataset)
 
     user_answers = Counter()
     user_best_answers = Counter()
@@ -82,67 +83,83 @@ def user_specific(dataset):
                 user_id, percentage, n_best, n_answer, n_ask), file=fout)
 
 
-def question_user(dataset):
+def question_user_content(dataset):
+    """
+    Generating user - answer and
+               user - question+answer contents
+
+    Args:
+        dataset  -  the dataset
+    """
     infile_a = DATA_DIR + "{}/Posts_A.json".format(dataset)
     infile_q = DATA_DIR + "{}/Posts_Q.json".format(dataset)
 
-    outfile = CUR_PARSED_DIR + "A.answer.content"
+    user_a_content = CUR_DIR + \
+                     "{}/user.answer.content".format(dataset)
+    user_aq_content = CUR_DIR + \
+                      "{}/user.question.answer.content".format(dataset)
 
-    generate_outfile = True
-    if not os.path.exists(CUR_PARSED_DIR):
-        os.mkdir(CUR_PARSED_DIR)
+    if not os.path.exists(CUR_DIR):
+        os.mkdir(CUR_DIR)
 
-    if os.path.exists(outfile):
+    if os.path.exists(user_a_content)\
+            and os.path.exists(user_aq_content):
         print("Answer content file exists. Skipping generation")
-        generate_outfile = False
+        return
 
-    if generate_outfile:
-        user_content = {}
-        sw_set = set(stopwords.words('english'))
+    content = {}
+    sw_set = set(stopwords.words('english'))
 
-        with open(infile_a, "r") as fin:
-            lines = fin.readlines()
-            for line in lines:
-                data = json.loads(line)
-                aid = data.get('OwnerId', None)
-                if not aid:
-                    continue
-                body = remove_stopwords(
-                        clean_str2(data['Body']), sw_set)
-                if aid not in user_content:
-                    user_content[aid] = [body]
-                else:
-                    user_content[aid].append(body)
+    with open(infile_a, "r") as fin:
+        lines = fin.readlines()
+        for line in lines:
+            data = json.loads(line)
+            aid = data.get('OwnerId', None)
+            if not aid:
+                continue
+            aid = int(aid)
+            body = remove_stopwords(
+                    clean_str2(data['Body']), sw_set)
+            if aid not in content:
+                content[aid] = body
+            else:
+                content[aid] += body
 
-        with open(infile_q, "r") as fin:
-            lines = fin.readlines()
-            for line in lines:
-                data = json.loads(line)
-                rid = data.get('OwnerId', None)
-                if not rid:
-                    continue
-                body = remove_stopwords(
-                        clean_str2(data['Body']), sw_set)
-                if rid not in user_content:
-                    user_content[rid] = [body]
-                else:
-                    user_content[rid].append(body)
+    with open(user_a_content, "wb") as fout:
+        pickle.dump(content, fout)
 
-        with open(outfile, "w") as fout:
-            for uid in user_content.keys():
-                text = " ".join(user_content[uid])
-                print("{} {}".format(uid, text), file=fout)
+    with open(infile_q, "r") as fin:
+        lines = fin.readlines()
+        for line in lines:
+            data = json.loads(line)
+            rid = data.get('OwnerId', None)
+            if not rid:
+                continue
+            rid = int(rid)
+            body = remove_stopwords(
+                    clean_str2(data['Body']), sw_set)
+            if rid not in content:
+                content[rid] = body
+            else:
+                content[rid] += body
 
-    # Later computation
-
-
+    with open(user_aq_content, "w") as fout:
+        pickle.dump(content, fout)
 
 
+def question_user_LM(dataset):
+    user_a_content = CUR_DIR + \
+                     "{}/user.answer.content".format(dataset)
+    user_aq_content = CUR_DIR + \
+                      "{}/user.question.answer.content".format(dataset)
 
+    # Loading two datasets
+    user_a, user_aq = {}, {}
+    with open(user_a_content, "rb") as fin:
+        user_a = pickle.load(fin)
 
-
-
-
+    with open(user_aq_content, "rb") as fin:
+        user_aq = pickle.load(fin)
 
 
 
--- tkde14/mat_optimize.py
+++ tkde14/mat_optimize.py
@@ -9,8 +9,86 @@ Implementing TKDE'14 paper:
     Expert Finding for Question Answering via Graph Regularized Matrix Completion
 """
 
+import cPickle
+
+from scipy.sparse import coo_matrix, save_npz
+
+import os, sys
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+
+try:
+    import ujson as json
+except:
+    import json
+
 # Global Variables
+DATA_DIR = os.getcwd() + "/data/"
+PARSED_DIR = DATA_DIR + "parsed/"
+CUR_DIR = os.getcwd() + "/tkde14/"
+CUR_PARSED_DIR = CUR_DIR + "parsed/"
+
+"""
+Three files to generate:
+    m: # of questions, 
+    n: # of users,
+    d: # of words in vocabulary
+    
+    1 - question.to.index: question ID to matrix index
+    2 - mat.Q (mat.Q.npz): question representation w.r.t words (d x m)
+    3 - mat.Y (mat.Y): 
+    
+"""
 
 
 def build_matrix_Q(dataset):
-    infile =
+    infile = PARSED_DIR + "{}/Q_content_nsw.txt".format(dataset)
+    mat_Q_file = CUR_DIR + "mat.Q"
+    question_index_file = CUR_DIR + "question.to.index"
+
+    count_vectorizer = CountVectorizer(
+        analyzer='word', stop_words='english', max_df=0.9, min_df=2)
+
+    with open(infile, "r") as fin,\
+        open(question_index_file, "w") as fout:
+        lines = fin.readlines()
+        lines = [line.split(" ", 1) for line in lines]
+        question_id_list = [entry[0] for entry in lines]
+        txt = [entry[1] for entry in lines]
+
+        print("\tFit-Transforming texts...", end= " ")
+        mat_Q = count_vectorizer.fit_transform(txt).toarray()
+        mat_Q = np.transpose(mat_Q)  # to make it d x m
+
+        for index, qid in enumerate(question_id_list):
+            print("{} {:d}".format(qid, index), file=fout)
+
+        sparse_mat_Q = coo_matrix(mat_Q)
+        save_npz(mat_Q_file, sparse_mat_Q)
+
+        print("Done!")
+
+
+def build_matrix_Y(dataset):
+    infile = DATA_DIR + "{}/Posts_A.json".format(dataset)
+    mat_Y_file = CUR_DIR + "mat.Y"
+
+    """
+    Format of matrix Y:
+        user_id question_id votes
+    """
+
+    with open(infile, "r") as fin,\
+        open(mat_Y_file, "w") as fout:
+        lines = fin.readlines()
+        for line in lines:
+            data = json.loads(line)
+            uid = data.get('OwnerUserId', None)
+            qid = data.get('ParentId', None)
+            score = data.get('Score', None)
+            if uid and qid and score:
+                print("{} {} {}".format(uid, qid, score), file=fout)
+
+
+def build_matrix_L(dataset):
+
